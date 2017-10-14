ffa139186865d81a2c5093964a87e499115bbd25
naren <makernaren@gmail.com>
2017-10-14 13:48:39 +0530

    Print server started message

--- main.go
+++ main.go
@@ -14,5 +14,6 @@ var Tokens map[string]*oauth.RequestToken
 func main() {
 	backend.NewOauth()
 	rt := hexserver.NewRouter()
+	log.Println("Server running at http://127.0.0.1:8000")
 	log.Fatal(http.ListenAndServe(":8000", rt))
 }
